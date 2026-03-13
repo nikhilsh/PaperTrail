7a527d6fa42a44bbf7b0dcadc8a636e096282b85
Nikhil Sharma <me@nikhil.sh>
2026-03-13 12:41:55 +0800

    asd

---
+++ PaperTrailTests/PaperTrailTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaperTrailTests.swift
+//  PaperTrailTests
+//
+//  Created by Nikhil Sharma on 13/3/26.
+//
+
+import Testing
+@testable import PaperTrail
+
+struct PaperTrailTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
